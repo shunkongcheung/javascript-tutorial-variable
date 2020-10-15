4986165a60763f530e1f55a9585f7efab3ae02c5
Shun Cheung <shunkongcheung@gmail.com>
2020-10-15 12:04:51 -0400

    feat(learnNumbers): init

---
+++ learnNumbers.js
@@ -0,0 +1,39 @@
+// Example of Integer and Float
+const int1 = 10;
+console.log(`Int1: ${int1}`)
+
+const float1 = 11.55;
+console.log(`float1: ${float1}`)
+
+const sum1 = int1 + 5 * 3; // try + - * /
+console.log(`Sum1 ${sum1}`)
+
+const sum2 = (int1 + 5)  * 3 // example of using brackets
+console.log(`Sum2 ${sum2}`)
+
+
+// increment
+let increment = 5;
+increment++; // also try increment--;
+console.log(`Increment(1): ${increment}`)
+
+++increment;
+console.log(`Increment(2): ${increment}`)
+
+
+// calcuation between float and int
+const cal1 = 5 / 6;
+console.log(`Calculation1: ${cal1}`)
+
+const cal2 = 5 + 1.1
+console.log(`Calculation2: ${cal2}`)
+
+
+// Math standard library
+const min = Math.min(5, 6) 
+console.log(`Min: ${min}`)
+
+const pi = Math.PI;
+console.log(`Pi: ${pi}`)
+
+
