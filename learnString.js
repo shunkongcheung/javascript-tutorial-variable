dea19abc0d20c8360223e376474456d8470360d7
Shun Cheung <shunkongcheung@gmail.com>
2020-10-15 11:55:07 -0400

    feat(learnString): init

---
+++ learnString.js
@@ -0,0 +1,28 @@
+// Using different syntax to define string
+let message1 = 'Hello World. I am message 1 with single quote'; // an example of using single quote
+console.log(message1)
+
+const message2 = "Hello world. I am message 2 with double quote"; // an example of using doulbe quote
+console.log(message2)
+
+const message3 = `Hello world. I am message 3 with backtick`; // an example of back tick
+console.log(message3)
+
+
+
+// Updaing Let variable
+message1 = 'Hello World. I am message 1 with single quote but updated' // an example of updaing "let" variable
+
+console.log(message1)
+
+
+
+// Concat strings
+const message4 = 'Hello ' + 'World. This is' + ' message 4';
+console.log(message4)
+
+const world = 'World';
+const msg5Name = 'message 5';
+const message5 = `Hello ${world}. This is ${msg5Name}`;
+console.log(message5)
+
